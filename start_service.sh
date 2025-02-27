dae165c329c31393ff5fd4a9ae834df279ae0f71
SammyAgrawal <samart.agr@gmail.com>
2025-02-27 01:15:01 -0500

    reverting volume storage to fix issues

--- start_service.sh
+++ start_service.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 if [ "$1" = "new" ]; then
     # Create new volume with timestamp
-    docker compose up
+    TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+    DB_VOLUME=postgres-data-$TIMESTAMP DB_VOLUME_EXTERNAL=false docker compose up
 elif [ -n "$1" ]; then
-    # Use specified volume
-    DB_VOLUME=$1 docker compose up
+    # Use specified existing volume
+    DB_VOLUME=$1 DB_VOLUME_EXTERNAL=true docker compose up
 else
-    echo "Usage: ./start-db.sh [new|volume-name]"
+    echo "Usage: ./start_service.sh [new|volume-name]"
     exit 1
 fi
